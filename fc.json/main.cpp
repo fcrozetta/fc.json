5fc454b7a7f835510d2e1e149d3c3faca8e669e0
Fernando Crozetta <fernando.crozetta@rabobank.com>
2024-10-21 15:08:15 +0200

    Initial Commit

---
+++ fc.json/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fc.json
+//
+//  Created by Fernando Crozetta on 21/10/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
